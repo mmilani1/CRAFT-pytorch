b0663502c9537325267bc16f9ef1861215c302f2
Matheus Milani <m_milani@hotmail.com>
2021-10-31 21:11:32 -0300

    Adding pre-trained model download script

---
+++ weights/download-pre-trained.sh
@@ -0,0 +1,7 @@
+# /bin/bash
+
+echo "Downloading craft.pth"
+wget --quiet --no-check-certificate 'https://drive.google.com/uc?id=1CJ9RJmsnKoor5tRG8CyptnTYsvNi7ZYv&export=download' -O craft.pth
+
+echo
+wget --quiet --no-check-certificate 'https://drive.google.com/uc?id=1dg-3qZ-ZTUqTqVrTvKlN-n3ZcxklbdT6&authuser=0&export=download' -O craft-refiner.pth
