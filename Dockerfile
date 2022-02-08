debaee86b2a12112f952218ff6f39eff42b66cd0
Matheus Milani <m_milani@hotmail.com>
2022-02-07 22:39:59 -0300

    Adding upload method for detection on custom image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM continuumio/miniconda3:latest
 
-ARG FLASK_ENV
+ARG FLASK_ENV='production'
 ENV FLASK_ENV $FLASK_ENV
 ENV FLASK_APP '/app/app.py'
-
-EXPOSE 5000
 
 WORKDIR /app
 
@@ -12,4 +10,4 @@ ADD . /app
 
 RUN conda env create -f /app/requirements-cpu.yml
 
-CMD ["conda", "run", "--no-capture-output", "-n", "craft-detector", "flask", "run", "--host=0.0.0.0"]
+CMD ["conda", "run", "--no-capture-output", "-n", "craft-detector", "flask", "run", "--host=0.0.0.0", "--port=$PORT"]
--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 from ocr_runner import OcrRunner
 
 app = Flask(__name__)
 
-@app.route('/hello-world')
-def hello_world():
-    return "Hello World"
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['image']
+        file_path = os.path.join('./upload', f.filename)
+        f.save(file_path)
+    
+        words = OcrRunner().run(file_path)
 
-@app.route('/ocr')
-def test():
-    words = OcrRunner().run("./images/img_2.jpg")
-    return ", ".join(words)
+        os.remove(file_path)
+        return ', '.join(words)
 
 if __name__ == '__main__':
     app.run()
